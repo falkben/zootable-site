d34ecb36c59d59fa95f508b452495e9527afe162
Ben Falk <falk.ben@gmail.com>
2020-08-13 11:58:02 -0400

    update nuxt (#11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  purge: ['./pages/**/*.html', './pages/**/*.vue', './pages/**/*.jsx'],
   theme: {
     maxWidth: {
       '1/4': '25%',
